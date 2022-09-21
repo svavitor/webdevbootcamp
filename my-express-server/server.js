fb5953e92634b75d6a14192672bae5a808c413f0
Vitor Senna <vitor.twist@gmail.com>
2022-09-21 19:20:01 -0300

    understanding and working with routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,1 +1,23 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res){
+  res.send("<h1>Hello, world!</h2>");
+});
+
+app.get("/contact", function(req, res){
+  res.send("Contact me at: contact@vvv.com");
+});
+
+app.get("/about", function(req, res){
+  res.send("My name is Vitor and I love gaming and code.");
+});
+
+app.get("/hobbies", function(req, res){
+  res.send("<ul><li>Coffee</li><li>Gaming</li><li>Code</li></ul>");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
