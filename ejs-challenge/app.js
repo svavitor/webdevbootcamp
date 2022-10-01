e593d0022c86659d12e8a3e1c510a3d62a353cdc
Vitor Senna <vitor.twist@gmail.com>
2022-10-01 17:14:12 -0300

    Challenges 1 to 5 completed

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -12,12 +12,20 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", function (req, res) {
+    res.render("home", { homeStartingContent: homeStartingContent });
+});
 
+app.get("/about", function (req, res) {
+    res.render("about", { aboutContent: aboutContent });
+});
 
-
+app.get("/contact", function (req, res) {
+    res.render("contact", { contactContent: contactContent})
+});
 
 
 
